fdd8ac5c6e5593e1cb5170bc3962f68850fea54d
vanilla120707 <131329871+vanilla120707@users.noreply.github.com>
2023-05-13 09:53:33 +0800

    MakeCode 專案的初始檔案

---
+++ main.ts
@@ -0,0 +1,9 @@
+basic.forever(function () {
+    basic.showIcon(IconNames.Surprised)
+    if (input.isGesture(Gesture.TiltLeft)) {
+        music.startMelody(music.builtInMelody(Melodies.Prelude), MelodyOptions.Once)
+    }
+    if (input.isGesture(Gesture.TiltRight)) {
+        music.startMelody(music.builtInMelody(Melodies.Entertainer), MelodyOptions.Once)
+    }
+})
